291fbed1dfbe4d41233bb4fc9bdc675285282b7b
kuyanov <43725417+kuyanov@users.noreply.github.com>
2020-01-01 01:54:43 +0300

    Fixed run_kafka.sh and run.py

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
+import os
 from app import app
 
+
+if not os.path.exists(app.config['SUBMISSIONS_EXEC_PATH']):
+    os.makedirs(app.config['SUBMISSIONS_EXEC_PATH'])
+if not os.path.exists(app.config['SUBMISSIONS_LOG_PATH']):
+    os.makedirs(app.config['SUBMISSIONS_LOG_PATH'])
+if not os.path.exists(app.config['SUBMISSIONS_DOWNLOAD_PATH']):
+    os.makedirs(app.config['SUBMISSIONS_DOWNLOAD_PATH'])
 
 app.run(host='0.0.0.0', port=3013, debug=True)
--- run_kafka.sh
+++ run_kafka.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-cd $1
-nohup bin/zookeeper-server-start.sh config/zookeeper.properties &
-nohup bin/kafka-server-start.sh config/server.properties &
-bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic judge
-echo "Completed!"
+echo -e "Stopping servers..."
+$1/bin/kafka-server-stop.sh
+$1/bin/zookeeper-server-stop.sh
+echo -e "Completed.\n"
+
+echo -e "Deleting /tmp/kafka-logs..."
+rm -rf /tmp/kafka-logs
+echo -e "Completed.\n"
+
+echo -e "Starting zookeeper..."
+$1/bin/zookeeper-server-start.sh -daemon $1/config/zookeeper.properties
+echo -e "Completed.\n"
+
+echo "Starting kafka server..."
+$1/bin/kafka-server-start.sh -daemon $1/config/server.properties
+echo -e "Completed.\n"
+
+echo -e "Creating topic judge (optional)..."
+$1/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic judge
+echo -e "Completed.\n"
+
 exit 0
