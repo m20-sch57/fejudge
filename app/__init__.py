0c66f234dbfa82ac4568bce4568a5907602f1ff6
kuyanov <43725417+kuyanov@users.noreply.github.com>
2020-07-27 15:20:01 +0300

    Fixed app logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,28 @@
+import os
+import logging
+
 from flask import Flask
 from flask_socketio import SocketIO
-from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from sqlalchemy import MetaData
 from pynats import NATSClient
 from config import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-socketio = SocketIO(app)
+socketio = SocketIO(app, logger=app.logger)
+
+file_handler = logging.FileHandler(app.config['LOG_FILE'])
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+app.logger.addHandler(file_handler)
+
 db = SQLAlchemy(app, metadata=MetaData(naming_convention=Config.NAMING_CONVENTION))
 migrate = Migrate(app, db, render_as_batch=True, compare_type=True)
+
 nats = NATSClient(Config.NATS_SERVER, name='producer1')
 
 login = LoginManager(app)
--- run.sh
+++ run.sh
@@ -2,14 +2,12 @@
 
 # Before running, specify MAIL_USERNAME and MAIL_PASSWORD of smtp server
 
-export FLASK_APP=app
-export FLASK_ENV=development
-export FLASK_DEBUG=true
-export DATA_DIR=$PWD/data
-
 if ! [ -d 'logs' ]; then
     mkdir logs
 fi
 
-nats-server -l logs/nats.log &
+export DATA_DIR=$PWD/data
+export FLASK_DEBUG=0
+
+nats-server --log logs/nats.log &
 python3 run.py
